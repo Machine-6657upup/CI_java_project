f10548b304b6f6922c13a80c316f4e545cbc6917
Machine-6657upup <60951175+Machine-6657upup@users.noreply.github.com>
2024-11-08 14:19:28 +0800

    Update Main.java

--- src/main/java/com/Main.java
+++ src/main/java/com/Main.java
@@ -1,7 +1,7 @@
-package com;
+// package com;
 
-import java.util.logging.Level;
-import java.util.logging.Logger;
+// import java.util.logging.Level;
+// import java.util.logging.Logger;
 
 public class Main {
 
