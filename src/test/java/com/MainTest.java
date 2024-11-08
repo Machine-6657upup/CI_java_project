ec6bed32a1771d76e61abd850e7ffcc17f969d54
Machine-6657upup <60951175+Machine-6657upup@users.noreply.github.com>
2024-11-08 14:24:30 +0800

    Update MainTest.java

--- src/test/java/com/MainTest.java
+++ src/test/java/com/MainTest.java
@@ -10,7 +10,9 @@ class MainTest {
 	void test() {
 		Main main = new Main();
 		int result = main.add(1, 1);
+		int result1 = main.add(999,999);
 		assertEquals(2, result);
+		assertEquals(1998, result1);
 	}
 
 }
